db1987ec2459106c3e5a526959d6b2c68f7e2f18
mirjalolkarimovich17-alt <mirjalolkarimovich17@gmail.com>
2026-07-01 17:05:21 +0300

    Add files via upload

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'OKS Suv Termiz',
+  description: 'Toza ichimlik suvi yetkazib berish xizmati',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="uz">
+      <body>{children}</body>
+    </html>
+  )
+}
